d1bd7fcdc13fedc3c0bc4765f6ca5a7ce226ffd0
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 <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>
2026-04-01 20:52:03 +0530

    wrong db env

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: parseInt(process.env.MYSQLPORT, 10) || 3306,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
